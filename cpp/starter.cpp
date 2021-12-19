8f3e6bb2d08e831b657c1f25562abeab6411b488
Lunfir <lunfirok@gmail.com>
2021-12-19 16:14:20 +0200

    Hello world

---
+++ cpp/starter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main()
+{
+    int number;
+
+    std::cin >> number;
+
+    std::cout << "Hello world!" << std::endl;
+    // std::cout << "Hello world!\n";
+
+    std::cout   << "number: " 
+                << number 
+                << std::endl;
+
+    return 0;
+}
