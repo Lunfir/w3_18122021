8c5d84ddf10cc42397fba55f2adcafd105a13916
Lunfir <lunfirok@gmail.com>
2021-12-19 19:39:19 +0200

    Struct: output operators

--- cpp/point/main.cpp
+++ cpp/point/main.cpp
@@ -46,26 +46,68 @@ struct Point
     {
         return hypot(this->x - other.x, this->y - other.y);
     }
+
+    // Point& operator+(const Point& other)
+    // {
+    //     std::cout << __PRETTY_FUNCTION__ << std::endl;
+    //     this->x += other.x;
+    //     this->y += other.y;
+
+    //     return *this;
+    // }
+
+    Point operator+(const Point& other)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        Point out;
+
+        out.x = this->x + other.x;
+        out.y = this->y + other.y;
+
+        return out;
+    }
+
+    Point& operator=(const Point& other)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        this->x = other.x;
+        this->y = other.y;
+
+        return *this;
+    }
 };
+
+std::ostream& operator<<(std::ostream& os, const Point& obj)
+{
+    os << "(" << obj.x << ", " << obj.y << ")";
+
+    return os;
+}
 
 
 int main()
 {
     // usual constructor
     Point pointA(12.2, 14.3);
-    // Point pointB = Point(10.2, 1.3);
+    Point pointB = Point(10.2, 1.3);
+    Point pointRes;
+
+    pointRes = pointB + pointA;
+    // pointRes.operator=(pointA.operator+(pointB));
 
     // copy constructor
     // Point pointB(pointA);
     // Point pointB = Point(pointA);
-    Point pointB = pointA;
 
-    pointB = pointA;
+    std::cout << "pointRes: " << pointRes << std::endl;
+    std::cout << "pointA:   " << pointA << std::endl;
+    std::cout << "pointB:   " << pointB << std::endl;
 
-    pointA.pointPrint();
-    pointB.pointPrint();
+    // pointRes.pointPrint();
+    // pointA.pointPrint();
+    // pointB.pointPrint();
 
-    std::cout << "is Equal: " << pointA.pointEqual(pointB) << std::endl;
+    // std::cout << "is Equal: " << pointA.pointEqual(pointB) << std::endl;
 
     return 0;
 }
